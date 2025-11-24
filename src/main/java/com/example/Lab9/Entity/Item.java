315450b3d5f3ae9dd63b925bde54cc82b09d64ab
KasymM3 <gaisin.k26@gmail.com>
2025-11-24 21:51:39 +0500

    A

---
+++ src/main/java/com/example/Lab9/Entity/Item.java
@@ -0,0 +1,25 @@
+package com.example.Lab9.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int price;
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "manufacturer_id")
+    private Country manufacturer;
+}
