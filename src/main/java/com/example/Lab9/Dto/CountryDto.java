ceeabca5f0d3db1d2c081c451d668e2eb501a16a
KasymM3 <gaisin.k26@gmail.com>
2025-11-25 12:45:08 +0500

    end

--- src/main/java/com/example/Lab9/Dto/CountryDto.java
+++ src/main/java/com/example/Lab9/Dto/CountryDto.java
@@ -1,11 +1,14 @@
 package com.example.Lab9.Dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CountryDto {
 
     private Long id;
--- src/main/java/com/example/Lab9/Dto/ItemDto.java
+++ src/main/java/com/example/Lab9/Dto/ItemDto.java
@@ -1,12 +1,15 @@
 package com.example.Lab9.Dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
 
     private Long id;
